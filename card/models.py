97759c9082a29070c7204ce4a6aeb4fb24b331ac
Marvelous-A <mogneto.r@mail.ru>
2023-10-09 22:40:39 +0300

    Changes to be committed: 	modified:   card/__pycache__/admin.cpython-311.pyc 	modified:   card/__pycache__/models.cpython-311.pyc 	modified:   card/__pycache__/urls.cpython-311.pyc 	modified:   card/admin.py 	new file:   card/migrations/0001_initial.py 	new file:   card/migrations/0002_alter_author_phone.py 	new file:   card/migrations/0003_author_validate_for_phonenambers.py 	new file:   card/migrations/0004_remove_author_validate_for_phonenambers_and_more.py 	new file:   card/migrations/__pycache__/0001_initial.cpython-311.pyc 	new file:   card/migrations/__pycache__/0002_alter_author_phone.cpython-311.pyc 	new file:   card/migrations/__pycache__/0003_author_validate_for_phonenambers.cpython-311.pyc 	new file:   card/migrations/__pycache__/0004_remove_author_validate_for_phonenambers_and_more.cpython-311.pyc 	modified:   card/models.py 	renamed:    card/static/css/card(2).css -> card/static/css/card.css 	modified:   card/templates/card/base.html 	modified:   card/urls.py 	modified:   db.sqlite3

--- card/models.py
+++ card/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Author(models.Model):
+    author_title = models.CharField(max_length=256, default='Фамилия имя')
+    discription = models.TextField(max_length=5000, default='Описание', null=True)
+    phone = models.CharField(max_length=20, default='799999999')
+    validate_for_phonenumbers = models.BooleanField(default=False)
+    age = models.IntegerField(null=True)
+    profession = models.CharField(max_length=300, null=True)
+
+    def validate_phonenumbers(self):
+        import phonenumbers
+        try:
+            parsed_number= phonenumbers.parse(self.phone, None)
+            if phonenumbers.is_valid_number(parsed_number):
+                self.validate_for_phonenumber = True
+            else: 
+                self.validate_for_phonenumbers = False
+        except phonenumbers.phonenumberutil.NumberParseException:
+            self.validate_for_phonenumber = False
+
+    def save(self, *args, **kwargs):
+        self.validate_phonenumbers()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return 'Данные на сайте'
